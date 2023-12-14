ec63ac025109112c8d78b369bb54bb0d63eb400a
Lufou <lucas.damase@laposte.net>
2023-12-14 21:14:28 +0100

    LUCID

--- traffic.py
+++ traffic.py
@@ -1,9 +1,14 @@
 from scapy.all import *
 import time
 import random
+import threading
+import signal
+
+# Event pour signaler aux threads de s'arrêter
+exit_event = threading.Event()
 
 def generate_normal_traffic(destination_ip="8.8.8.8"):
-    while True:
+    while not exit_event.is_set():
         # Créer un paquet ICMP (ping)
         packet = IP(dst=destination_ip)/ICMP()
 
@@ -14,22 +19,22 @@ def generate_normal_traffic(destination_ip="8.8.8.8"):
         time.sleep(random.uniform(0.5, 2))
 
 def simulate_ddos_attack(destination_ip="8.8.8.8"):
-    while True:
+    while not exit_event.is_set():
         # Créer un paquet ICMP (ping) pour une attaque DDoS (ping flood)
         packet = IP(dst=destination_ip)/ICMP(type=8, code=0)
 
         # Envoyer le paquet
         send(packet, loop=True, inter=0.01)  # Envoi en boucle avec un intervalle très court
 
-def stop_sniffing(signal, frame):
-    print("CTRL+C detected. Stopping traffic...")
-    exit(0)
-
+def signal_handler(sig, frame):
+    print("Arrêt du script.")
+    exit_event.set()
 
 def main():
-    import signal
-    signal.signal(signal.SIGINT, stop_sniffing)
     destination_ip = "8.8.8.8"
+
+    # Capturer le signal CTRL+C pour permettre l'arrêt propre du script
+    signal.signal(signal.SIGINT, signal_handler)
 
     # Générer du trafic normal en parallèle avec la simulation d'attaque DDoS
     normal_traffic_thread = threading.Thread(target=generate_normal_traffic, args=(destination_ip,))
